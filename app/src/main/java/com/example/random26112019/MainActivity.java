b049e85d36b35a7e02d0288e5e55c9efc58f11b2
phatdroid94 <phatdroid94@gmail.com>
2019-12-12 18:15:42 +0700

    Math

--- app/src/main/java/com/example/random26112019/MainActivity.java
+++ app/src/main/java/com/example/random26112019/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,19 @@ public class MainActivity extends AppCompatActivity {
 //        0.1 * 5 = 0.5
 //        0.2 * 5= 1
 //        0.9 * 5 = 4.5
-        double random = Math.random() * 5;
+//        double random = Math.random() * 5;
         // Lam tron gia tri
 //        double number1 = Math.round(random);
         //Lam tron len
-        double number2 = Math.ceil(random);
-        Log.d("BBB",String.valueOf(random));
-        Log.d("BBB",String.valueOf(number2));
+//        double number2 = Math.ceil(random);
+        //Lam tron xuong
+//        double number2 = Math.floor(random);
+//        Log.d("BBB",String.valueOf(random));
+//        Log.d("BBB",String.valueOf(number2));
+//        int a = 5;
+//        int b = 10;
+//        int ketqua = Math.max(a , b);
+//        Log.d("BBB",String.valueOf(ketqua));
+        Random random = new Random();
     }
 }
